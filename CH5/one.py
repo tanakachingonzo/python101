7c0ed07c4517a6f72776db76516419b7f5d63abf
Tanaka Chingonzo <tanaching@gmail.com>
2017-10-25 11:36:27 -0400

    Python to print minimum and maximum value from a list

--- CH5/one.py
+++ CH5/one.py
@@ -1,14 +1,18 @@
 in_File=open(r'ch5rainfall.txt', 'r')
 out_File = open(r'resukts.txt', 'w')
 
-
+x=[]
 for line in in_File:
     list21 = line.split(" ")
     # print( list21 )
     out_File.write(list21[0]+" " + "has this much rain:"+ list21[1] + "\n")
-out_File.write("City with max rain:" + "+maxRainCity+" + "\n")
+    x.append(float(list21[1]))
 
+maximumval =str(max(x))
+minimumval =str(min(x))
 
+out_File.write("City with max rain:" + maximumval + "\n")
+out_File.write("City with min rain:" + minimumval + "\n")
 
 in_File.close()
 out_File.close()
